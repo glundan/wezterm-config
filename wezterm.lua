b7b2909a946e1ea0d10a97876a0c36968115f98c
Gabriel Undan <undan.gabriel@gmail.com>
2025-04-09 22:07:30 -1000

    powershell at startup on windows; font size lowered

--- wezterm.lua
+++ wezterm.lua
@@ -1,8 +1,15 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
+local os = wezterm.target_triple
+if os == "x86_64-pc-windows-msvc" then
+	config.default_prog = { "powershell.exe" }
+end
+
+
 config.color_scheme = "carbonfox"
 config.font = wezterm.font("0xProto Nerd Font Mono")
-config.font_size = 16
+config.font_size = 14
+
 
 return config
