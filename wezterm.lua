69cd13492949e2914fef36854168b0d3ec6e2007
Gabriel Undan <undan.gabriel@gmail.com>
2025-08-19 14:12:31 -1000

    Added binds for OPT+Left and Right Arrow

--- wezterm.lua
+++ wezterm.lua
@@ -1,15 +1,33 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
+local act = wezterm.action
 
 local os = wezterm.target_triple
 if os == "x86_64-pc-windows-msvc" then
 	config.default_prog = { "powershell.exe" }
 end
 
-
 config.color_scheme = "carbonfox"
 config.font = wezterm.font("0xProto Nerd Font Mono")
 config.font_size = 14
 
+config.keys = {
+	{
+		key = "LeftArrow",
+		mods = "OPT",
+		action = act.SendKey({
+			key = "b",
+			mods = "ALT",
+		}),
+	},
+	{
+		key = "RightArrow",
+		mods = "OPT",
+		action = act.SendKey({
+			key = "f",
+			mods = "ALT",
+		}),
+	},
+}
 
 return config
